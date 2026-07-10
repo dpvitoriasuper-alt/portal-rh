d1e1907a14ae2bc26dd89873673f8ab9b010301d
dpvitoriasuper-alt <dpvitoriasuper@gmail.com>
2026-07-10 00:20:37 -0400

    Update 03_ModalManager.js

--- 03_ModalManager.js
+++ 03_ModalManager.js
@@ -5,18 +5,116 @@
 
 function atualizarBody(){
 
-    if(modaisAbertos > 0){
+    const ModalSCRV = (() => {
 
-        document.body.classList.add("modal-aberto");
-        document.body.style.overflow = "hidden";
+    let zAtual = 10000;
 
-    }else{
+    let modaisAbertos = 0;
 
-        document.body.classList.remove("modal-aberto");
-        document.body.style.overflow = "";
+    function atualizarBody(){
 
-        document.documentElement.style.overflow = "";
+        if(modaisAbertos > 0){
+
+            document.body.classList.add("modal-aberto");
+
+            document.body.style.overflow = "hidden";
+
+        }else{
+
+            document.body.classList.remove("modal-aberto");
+
+            document.body.style.overflow = "";
+
+            document.documentElement.style.overflow = "";
+
+        }
+    }
+
+    function abrir(id){
+
+        const modal = document.getElementById(id);
+
+        if(!modal){
+
+            console.error("Modal não encontrado:", id);
+
+            return;
+
+        }
+
+        if(modal.style.display !== "flex"){
+
+            modaisAbertos++;
+
+        }
+
+        zAtual++;
+
+        modal.style.display = "flex";
+
+        modal.style.zIndex = zAtual;
+
+        atualizarBody();
 
     }
 
-}
+    function fechar(id){
+
+        const modal = document.getElementById(id);
+
+        if(!modal) return;
+
+        if(modal.style.display === "flex"){
+
+            modal.style.display = "none";
+
+            modaisAbertos--;
+
+            if(modaisAbertos < 0){
+
+                modaisAbertos = 0;
+
+            }
+
+        }
+
+        atualizarBody();
+
+    }
+
+    function fecharTodos(){
+
+        document
+
+        .querySelectorAll(".modal-rh,.documentos-modal")
+
+        .forEach(modal=>{
+
+            modal.style.display="none";
+
+        });
+
+        modaisAbertos = 0;
+
+        atualizarBody();
+
+    }
+
+    function quantidade(){
+        return modaisAbertos;
+
+    }
+
+    return{
+
+        abrir,
+
+        fechar,
+
+        fecharTodos,
+
+        quantidade
+
+    };
+
+})();
