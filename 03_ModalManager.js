37829aaec25a998dc1169e41750a32b434eba4a1
dpvitoriasuper-alt <dpvitoriasuper@gmail.com>
2026-07-10 00:16:54 -0400

    Update 03_ModalManager.js

--- 03_ModalManager.js
+++ 03_ModalManager.js
@@ -3,37 +3,19 @@
  * Modal Manager v2
  **********************************************************************/
 
-function atualizarBody() {
+function atualizarBody(){
 
-    const existeModalAberto = Array.from(
-        document.querySelectorAll(".modal-rh, .documentos-modal")
-    ).some(modal => {
-
-        const estilo = window.getComputedStyle(modal);
-
-        return (
-            estilo.display !== "none" &&
-            estilo.visibility !== "hidden" &&
-            parseFloat(estilo.opacity) > 0
-        );
-
-    });
-
-    if (existeModalAberto) {
+    if(modaisAbertos > 0){
 
         document.body.classList.add("modal-aberto");
         document.body.style.overflow = "hidden";
-        document.documentElement.style.overflow = "hidden";
 
-    } else {
+    }else{
 
         document.body.classList.remove("modal-aberto");
         document.body.style.overflow = "";
-        document.documentElement.style.overflow = "";
 
-        // Sincroniza o contador caso alguém tenha aberto/fechado modal
-        // diretamente com style.display
-        modaisAbertos = 0;
+        document.documentElement.style.overflow = "";
 
     }
 
