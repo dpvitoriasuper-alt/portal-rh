d266eb6e46c1b46da1d97cb4dbb82de9f757d93e
dpvitoriasuper-alt <dpvitoriasuper@gmail.com>
2026-07-11 19:08:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -641,6 +641,16 @@ function fecharFuncionariosRh(){
 }
  async function pesquisarFuncionariosRh(){
 
+   console.log("=== NOVA PESQUISA RH ===");
+
+  const input = document.getElementById("pesquisaFuncionarioRh");
+  console.log("INPUT:", input);
+
+  const tabela = document.getElementById("resultadoFuncionariosRh");
+  console.log("TABELA:", tabela);
+
+  const termo = input.value.trim();
+
   const termo = document.getElementById("pesquisaFuncionarioRh").value.trim();
   const corpoTabela = document.getElementById("resultadoFuncionariosRh");
 
