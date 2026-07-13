803a4e8ac38a414914b45094e08d22977161b6fd
dpvitoriasuper-alt <dpvitoriasuper@gmail.com>
2026-07-12 23:14:25 -0400

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,1 +1,27 @@
+/**********************************************************************
+ * SCRV - Core
+ **********************************************************************/
 
+window.SCRV = window.SCRV || {
+
+    API: {},
+
+    Utils: {},
+
+    Estado: {},
+
+    RH: {
+        Funcionarios: {},
+        Cadastro: {},
+        Documentos: {},
+        Historico: {},
+        Transferencia: {},
+        Experiencia: {},
+        Desligados: {},
+        Importacao: {},
+        Relatorios: {}
+    },
+
+    Colaborador: {}
+
+};
