d202750f5f739f9b7d56e9255b7bf3b824d06f5d
dpvitoriasuper-alt <dpvitoriasuper@gmail.com>
2026-07-13 23:05:44 -0400

    Update whatsapp.js

--- js/portal/whatsapp.js
+++ js/portal/whatsapp.js
@@ -1,1 +1,31 @@
+const TELEFONES_RH_LOJAS = {
+  COROADO: "5592982554733",
+  CENTRO: "5592981708226",
+  SAO_JOSE: "5592982352382",
+  TURISMO: "5592988170568",
+  TORQUATO: "5592992772611",
+  JAPIIM: "5592993454607",
+  GRANDE_CIRCULAR: "5592982285909"
+};
+function abrirModalLojas(){
+  document.getElementById("modalLojas").style.display = "flex";
+}
 
+function fecharModalLojas(){
+  document.getElementById("modalLojas").style.display = "none";
+}
+
+function abrirWhatsappLoja(loja){
+  const telefone = TELEFONES_RH_LOJAS[loja];
+
+  if(!telefone){
+    alert("Telefone da loja não encontrado.");
+    return;
+  }
+
+  const mensagem = "Olá, RH. Vim pelo Portal do Colaborador.";
+  const url = "https://wa.me/" + telefone + "?text=" + encodeURIComponent(mensagem);
+
+  window.open(url, "_blank");
+}
+  
