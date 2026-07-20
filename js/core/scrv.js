467b29538d41477bfd2a2ffd634ee00887279794
dpvitoriasuper-alt <dpvitoriasuper@gmail.com>
2026-07-19 20:47:51 -0400

    Create scrv.js

---
+++ js/core/scrv.js
@@ -0,0 +1,18 @@
+/**
+ * ============================================================
+ * SCRV - Sistema Corporativo Rede Vitória
+ * Núcleo do Sistema
+ * ============================================================
+ */
+
+window.SCRV = window.SCRV || {};
+
+SCRV.version = "1.0.0";
+
+SCRV.Core = {};
+
+SCRV.Utils = {};
+
+SCRV.UI = {};
+
+SCRV.Processamento = {};
