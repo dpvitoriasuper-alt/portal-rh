5316e95f36f796b6925802213d0d1325a565e998
dpvitoriasuper-alt <dpvitoriasuper@gmail.com>
2026-07-19 22:08:55 -0400

    Update processamento.js

--- js/processamento.js
+++ js/processamento.js
@@ -1,1 +1,79 @@
+/**
+ * ==========================================================
+ * SCRV.Processamento
+ * ==========================================================
+ */
 
+SCRV.Processamento = {
+
+    id: null,
+
+    timer: null,
+
+    ultimoSeq: 0,
+
+    ativo: false,
+
+    dados: {}
+
+};
+SCRV.Processamento.iniciar = function(config){
+
+    this.id = config.id;
+
+    this.ultimoSeq = 0;
+
+    this.ativo = true;
+
+    this.dados = {};
+
+    this.abrir();
+
+    this.monitorar();
+
+};
+SCRV.Processamento.monitorar = function(){
+
+    if(this.timer){
+
+        clearInterval(this.timer);
+
+    }
+
+    this.timer = setInterval(async ()=>{
+
+        await this.buscarStatus();
+
+    },2000);
+
+};
+SCRV.Processamento.buscarStatus = async function(){
+
+    const resposta = await SCRV.Http.post({
+
+        action:"consultarProcessamentoSCRV",
+
+        id:this.id,
+
+        ultimoSeq:this.ultimoSeq
+
+    });
+
+    if(!resposta.sucesso){
+
+        return;
+
+    }
+
+    this.render(resposta);
+
+};
+SCRV.Processamento.encerrar = function(){
+
+    this.ativo=false;
+
+    clearInterval(this.timer);
+
+    this.timer=null;
+
+};
