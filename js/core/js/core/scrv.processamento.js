b04135216261af7bd2496278b756b36743a203bb
dpvitoriasuper-alt <dpvitoriasuper@gmail.com>
2026-07-19 20:48:48 -0400

    Create scrv.processamento.js

---
+++ js/core/js/core/scrv.processamento.js
@@ -0,0 +1,38 @@
+/**
+ * ============================================================
+ * SCRV.Processamento
+ * Central de Processamento do SCRV
+ * ============================================================
+ */
+
+SCRV.Processamento = {
+
+    dados: {
+
+        ativo: false,
+
+        tipo: "",
+
+        titulo: "",
+
+        arquivo: "",
+
+        percentual: 0,
+
+        paginaAtual: 0,
+
+        totalPaginas: 0,
+
+        processados: 0,
+
+        pendencias: 0,
+
+        erros: 0,
+
+        tempo: "00:00",
+
+        logs: []
+
+    }
+
+};
