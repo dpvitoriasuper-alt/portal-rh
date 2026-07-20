6be23abe77542db27eb48545a09d0ddf0025ef1a
dpvitoriasuper-alt <dpvitoriasuper@gmail.com>
2026-07-19 21:49:40 -0400

    Update scrv.modal.js

--- scrv.modal.js
+++ scrv.modal.js
@@ -1,1 +1,37 @@
+/**
+ * =====================================================
+ * SCRV.UI
+ * Componentes visuais do SCRV
+ * =====================================================
+ */
+
+SCRV.UI = SCRV.UI || {};
+
+SCRV.UI.Modal = {};
+SCRV.UI.Modal.abrir = function(id){
+
+    const modal = document.getElementById(id);
+
+    if(!modal) return;
+
+    modal.style.display = "flex";
+
+    modal.classList.add("scrv-modal-open");
+
+}
+SCRV.UI.Modal.fechar = function(id){
+
+    const modal = document.getElementById(id);
+
+    if(!modal) return;
+
+    modal.classList.remove("scrv-modal-open");
+
+    setTimeout(()=>{
+
+        modal.style.display="none";
+
+    },180);
+
+}
 
